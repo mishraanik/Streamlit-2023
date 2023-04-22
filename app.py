9d63451dffba6114a81758591558d4c57946c4cc
Aniket Mishra <87333320+mishraanik@users.noreply.github.com>
2023-04-22 19:27:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import os
 
-st.header("Hello Hope so you are enjoying")
-st.header("I'm Learning Streamlit")
+st.header('STREAMLIT :red[colors] Tutorial')
+
+st.subheader('Hey People You doing fine ??')
+
+st.title('This is a title')
+st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
+st.title('A title with _BOLD_ :red[colors] and emojis :desert:')
+
+st.caption('This is a string that explains something above.')
+st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
+
+ASDF = ''' print ("Hello LAB")'''
+
+st.code(ASDF, language="python")
